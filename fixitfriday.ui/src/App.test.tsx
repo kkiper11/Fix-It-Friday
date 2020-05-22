69e98a897db93740df2c19043c8ed216d27aeae7
Luis Richmond <lalrg@outlook.com>
2020-05-22 09:54:11 -0600

    Add TS support

--- fixitfriday.ui/src/App.test.tsx
+++ fixitfriday.ui/src/App.test.tsx
@@ -5,5 +5,5 @@ import App from './App';
 test('renders Welcome text', () => {
   const { getByText } = render(<App />);
   const textElement = getByText(/Welcome To Fix it Fridays/i);
-  expect(textElement).toBeInTheDocument();
+  expect(textElement).toBeTruthy();
 });
